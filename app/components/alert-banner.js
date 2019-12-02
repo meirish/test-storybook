ad1f1ae76e345c2c7aacbe4c7f921470df2f24ae
Matthew Irish <matthew@hashicorp.com>
2019-12-02 10:33:00 -0600

    add different types of components with different types of code comments

--- app/components/alert-banner.js
+++ app/components/alert-banner.js
@@ -2,22 +2,40 @@ import Component from '@ember/component';
 import layout from '../templates/components/alert-banner';
 
 /**
- * @module AlertBanner
  * `AlertBanner` components are used to inform users of important messages.
  *
- * @example
  * ```js
  * <AlertBanner @type="danger" @message="{{model.keyId}} is not a valid lease ID"/>
  * ```
  *
- * @argument type=null {String} - The banner type. This comes from the message-types helper.
- * @argument [message=null {String}] - The message to display within the banner.
- * @argument [title=null {String}] - A title to show above the message. If this is not provided, there are default values for each type of alert.
+ * @class AlertBanner
+ */
+
+
+/**
+ * @argument message
+ * @default null
+ * @type string
+ * @description The message to display within the banner.
+ */
+
+/**
+ * @argument title
+ * @required
+ * @type string
+ * @description A title to show above the message. If this is not provided, there are default values for each type of alert.
  *
  */
 
 export default Component.extend({
   layout,
+/**
+ * @argument type
+ * @type string
+ * @default null
+ * @required
+ * @description The banner type. This comes from the message-types helper.
+ */
   type: null,
   message: null,
   title: null,
